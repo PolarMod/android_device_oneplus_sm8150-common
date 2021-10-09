2492b5b86959db3bf9e4902b441a318d501a3577
Andrewerr <toddot@live.ru>
2021-10-09 18:47:38 +0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -513,3 +513,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.use_color_management=true \
     ro.surface_flinger.wcg_composition_dataspace=143261696
+
+# OEM Unlock reporting
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
